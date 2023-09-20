2019434d1b9642b8d516dd286c9bbb5f4bdc2d12
matteoNapoli42 <matteon4242@gmail.com>
2023-09-20 17:29:49 +0200

    now print on dom

--- main.js
+++ main.js
@@ -7,16 +7,17 @@ createApp({
     },
     mounted(){
         let i = 0;
+        let arr=[];
         while(i<10)
         {
+            
             i++;
             axios.get(' https://flynn.boolean.careers/exercises/api/random/mail')
             .then(response =>{
-                    console.log(response.data.response);
+                     this.emailList.push(response.data.response);
                 }
             )
         }
-       
     },
     }
   ).mount('#app')
